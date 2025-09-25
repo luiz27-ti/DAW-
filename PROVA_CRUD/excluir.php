7af5af6603f2f28c9400417892362e9556e9813b
Luiz Gustavo Coelho Xavier <luizgustavocx1981@gmail.com>
2025-09-25 15:42:47 -0300

    Update excluir.php

--- PROVA_CRUD/excluir.php
+++ PROVA_CRUD/excluir.php
@@ -1,1 +1,29 @@
+<?php
+$arquivo = "3DAW_Luiz";
+$msg = "";
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $linhas = file($arquivo);
+
+    if (isset($linhas[$id])) {
+        unset($linhas[$id]); 
+        file_put_contents($arquivo, implode("", $linhas));
+        $msg = "Pergunta excluída com sucesso!";
+    } else {
+        $msg = "Pergunta não encontrada.";
+    }
+} else {
+    $msg = "Nenhum ID informado.";
+}
+?>
+<!DOCTYPE html>
+<html>
+<head><title>Excluir Pergunta</title></head>
+<body>
+<h1>Excluir Pergunta</h1>
+<p style="color:green;"><?php echo $msg; ?></p>
+<a href="listarTodas.php">⬅ Voltar para lista</a>
+</body>
+</html>
 
